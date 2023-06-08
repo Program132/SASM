8d29cdda54a37355185c3bb81513c88d202838ed
Program132 <clement.gauthier132@orange.fr>
2023-06-08 16:44:20 +0200

    Class for TOKENS

---
+++ src/Tokenizer/TokenClass.cpp
@@ -0,0 +1,74 @@
+#include "TokenClass.h"
+
+std::ostream &SASM::Tokenizer::operator<<(std::ostream &flux, const SASM::Tokenizer::Token &token) {
+    flux << "Token: " << token.TokenContent << ", type: " << sAllTokenTypes[token.TokenType];
+    return flux;
+}
+
+namespace SASM::Tokenizer {
+
+    std::vector<Token> Builder::ParserTokens(std::string_view code) {
+        std::vector<Token> Tokens;
+        Token current_token;
+
+        for (auto const &element : code) {
+            switch (element) {
+                case '9':
+                case '8':
+                case '7':
+                case '6':
+                case '5':
+                case '4':
+                case '3':
+                case '2':
+                case '1':
+                case '0':
+                    if (current_token.TokenType == WHITESPACE) {
+                        current_token.TokenType = INT;
+                        current_token.TokenContent.append(1, element);
+                    } else if (current_token.TokenType == IDENTIFIANT) {
+                        OverToken(current_token, Tokens);
+                        current_token.TokenType = INT;
+                        current_token.TokenContent.append(1, element);
+                    } else {
+                        current_token.TokenContent.append(1, element);
+                    }
+                    break;
+
+                case '#':
+                case ':':
+                case ',':
+                    OverToken(current_token, Tokens);
+                    current_token.TokenType = OPERATOR;
+                    current_token.TokenContent.append(1, element);
+                    OverToken(current_token, Tokens);
+                    break;
+
+                case ' ':
+                case '\t':
+                    OverToken(current_token, Tokens);
+                    break;
+
+                default:
+                    if (current_token.TokenType == WHITESPACE || current_token.TokenType == INT) {
+                        OverToken(current_token, Tokens);
+                        current_token.TokenType = IDENTIFIANT;
+                        current_token.TokenContent.append(1, element);
+                    } else {
+                        current_token.TokenContent.append(1, element);
+                    }
+                    break;
+            }
+        }
+        return Tokens;
+    }
+
+    void Builder::OverToken(Token &token, std::vector<Token> &tokens) {
+        if (token.TokenType != WHITESPACE) {
+            tokens.push_back(token);
+        }
+
+        token.TokenType = WHITESPACE;
+        token.TokenContent.erase();
+    }
+}
---
+++ src/Tokenizer/TokenClass.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string_view>
+
+namespace SASM::Tokenizer {
+    enum Types {
+        WHITESPACE,
+        IDENTIFIANT,
+        INT,
+        OPERATOR,
+    };
+
+    inline constexpr const char *sAllTokenTypes[] {
+            "WHITESPACE",
+            "IDENTIFIANT",
+            "INT",
+            "OPERATOR",
+    };
+
+    class Token {
+    public:
+        enum Types TokenType = WHITESPACE;
+        std::string TokenContent;
+
+        friend std::ostream& operator<<(std::ostream& flux, Token const& token);
+    };
+
+    class Builder {
+    public:
+        static std::vector<Token> ParserTokens(std::string_view code);
+        static void OverToken(Token& token, std::vector<Token>& tokens);
+    };
+}

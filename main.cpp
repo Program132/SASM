bf65702ba2e31983b61147e2168ddc17a9fffdc3
Program132 <clement.gauthier132@orange.fr>
2023-06-08 16:44:11 +0200

    Main cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "src/Tokenizer/TokenClass.h"
+
+int main(int argc, char** argv) {
+    std::string pos_FileName;
+    if (argc == 2) {
+        pos_FileName = argv[1];
+    } else {
+        std::cout << "File : ";
+        std::cin >> pos_FileName;
+        std::cout << std::endl;
+    }
+
+    std::ifstream possibleFile {pos_FileName};
+    if (!possibleFile) {
+        std::cerr << "Did not find the file!" << std::endl;
+        return -1;
+    }
+
+    std::string contentFile((std::istreambuf_iterator<char>(possibleFile)), (std::istreambuf_iterator<char>()));
+
+    std::vector<SASM::Tokenizer::Token> TokensCode = SASM::Tokenizer::Builder::ParserTokens(contentFile);
+
+    for (auto const& e : TokensCode) {
+        std::cout << e << std::endl;
+    }
+
     return 0;
 }
